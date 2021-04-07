04b2b0e34c71d48375f9a6913a475de0183ab481
m_costa3@etu-webschoolfactory.fr <PTg]m3vH6prx7QNGU2}823]}%yd2>
2021-04-07 18:31:38 +0200

    form new category added

--- src/main/java/com/msagres/inventoryapp/category/CategoryController.java
+++ src/main/java/com/msagres/inventoryapp/category/CategoryController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
 
@@ -13,9 +14,22 @@ public class CategoryController {
     private CategoryRepository repository;
 
     @GetMapping("/categories")
-    public String listCategories(Model model){
+    public String listCategories(Model model) {
         List<Category> categoryList = repository.findAll();
         model.addAttribute("categoryList", categoryList);
         return "categories";
+    }
+
+    @GetMapping("/categories/new")
+    public String showCategoryNewForm(Model model) {
+        model.addAttribute("category", new Category());
+
+        return "category_form";
+    }
+
+    @PostMapping("/categories/save")
+    public String saveCategory(Category category) {
+        repository.save(category);
+        return "redirect:/categories";
     }
 }
